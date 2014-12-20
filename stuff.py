b901a558964f3ede542e9c5307d2efc2d3a148bf
evuez <evuez@ares>
2014-12-20 13:36:51 +0100

    add some things

--- stuff.py
+++ stuff.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from copy import deepcopy
 from math import sqrt
+from random import shuffle
+from struct import unpack
 
 
 Point = namedtuple('Point', 'x y')
 
 
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 600
 
 MAP_WIDTH = 400
 MAP_HEIGHT = 400
@@ -16,25 +19,38 @@ MAP_HEIGHT = 400
 THING_SIZE = 4
 
 
-def color_variant(color, scale=1.0):
+def lighten(color, scale=1.0):
 	"""
-	Darken or lighten a color.
+	Lighten a color.
 	 - color is a tuple (r, g, b, a)
-	 - scale can be any number
+	 - scale can be any number, if < 1, color will be darken
 	"""
 	return map(
-		lambda x: int(min(max(x * scale, 0), 255)),
+		lambda x: min(max(x * scale, 0), 1),
 		color[:3]
 	) + list(color[3:])
 
 
-def matrix_center(matrix):
-	return (len(matrix) / 2, len(matrix[0]) / 2)
+def hex_to_rgb(hex_):
+	return [v / 255.0 for v in unpack('BBBB', hex_.decode('hex'))]
 
 
-def matrix_distance(x1, y1, x2, y2):
-	return (x2 - x1, y2 - y1)
+def matrix_distance(x1, y1, x2, y2, ratio=1):
+	return [n * ratio for n in (x2 - x1 , y2 - y1)]
 
 
-def distance(x1, y1, x2, y2):
-	return sqrt(sum([n**2 for n in matrix_distance(x1, y1, x2, y2)]))
+def shuffle_matrix(matrix, rows_only=False):
+	"""
+	Perform in-place shuffling
+	"""
+	matrix = deepcopy(matrix)
+	if not rows_only:
+		shuffle(matrix)
+	for row in matrix:
+		shuffle(row)
+	return matrix
+
+
+def distance(x1, y1, x2, y2, ratio=1):
+	return sqrt(sum([n**2 for n in matrix_distance(x1, y1, x2, y2, ratio)]))
+
--- things.py
+++ things.py
@@ -1,19 +1,57 @@
 # -*- coding: utf-8 -*-
 
 
-from pyglet.sprite import Sprite
+from pyglet.gl import glBegin
+from pyglet.gl import glColor4f
+from pyglet.gl import glVertex2f
+from pyglet.gl import glEnd
+from pyglet.gl import GL_POLYGON
+from stuff import THING_SIZE
+from stuff import matrix_distance
+from stuff import shuffle_matrix
+from stuff import lighten
+from stuff import hex_to_rgb
 
 
 class Thing(object):
 	"""
 	Every thing as only one color, and are
 	represented by a matrix of any size.
-	 - MATRIX is a tuple of tuple(s) of heights (0 being the ground).
+	 - MATRIX is a tuple of tuple(s) of heights (0 being the ground). The
+	number of rows and columns must be an odd number. Note that they are
+	rotated 90 degrees counterclockwise.
 	 - COLOR is the base color of the thing, real color is calculated
-	using heights
+	using heights. It must contain the alpha channel.
+	 - location set the top left corner of the first cell of the thing.
 	"""
 	MATRIX = None
 	COLOR = None
+
+	def __init__(self, location):
+		self.location = location
+
+	def draw(self):
+		for x, row in enumerate(self.MATRIX):
+			for y, height in enumerate(row):
+				if height is None:
+					continue
+				dist = matrix_distance(0, 0, x, y, THING_SIZE)
+				self.draw_cell(
+					lighten(self.COLOR, height),
+					self.location.x + dist[0],
+					self.location.y + dist[1]
+				)
+
+
+	def draw_cell(self, color, x1, y1):
+		x2, y2 = x1 + THING_SIZE, y1 + THING_SIZE
+		glColor4f(*color)
+		glBegin(GL_POLYGON)
+		glVertex2f(x1, y1)
+		glVertex2f(x1, y2)
+		glVertex2f(x2, y2)
+		glVertex2f(x2, y1)
+		glEnd()
 
 
 class Tree(Thing):
@@ -23,15 +61,57 @@ class Tree(Thing):
 class Pine(Tree):
 	MATRIX = [
 		[None, None, None, None, None,    1, None, None, None, None, None],
-		[None, None, None, None,    1,    2,    1, None, None, None, None],
-		[None, None, None,    1,    2,    3,    2,    1, None, None, None],
-		[None, None,    1,    2,    3,    4,    3,    2,    1, None, None],
-		[None,    1,    2,    3,    4,    5,    4,    3,    2,    1, None],
-		[   1,    2,    3,    4,    5,    6,    5,    4,    3,    2,    1],
-		[None,    1,    2,    3,    4,    5,    4,    3,    2,    1, None],
-		[None, None,    1,    2,    3,    4,    3,    2,    1, None, None],
-		[None, None, None,    1,    2,    3,    2,    1, None, None, None],
-		[None, None, None, None,    1,    2,    1, None, None, None, None],
+		[None, None, None, None,    1,    1,    1, None, None, None, None],
+		[None, None, None,    1,    1,    2,    1,    1, None, None, None],
+		[None, None,    1,    1,    2,    2,    2,    1,    1, None, None],
+		[None,    1,    1,    2,    2,    3,    2,    2,    1,    1, None],
+		[   1,    1,    2,    2,    3,    4,    3,    2,    2,    1,    1],
+		[None,    1,    1,    2,    2,    3,    2,    2,    1,    1, None],
+		[None, None,    1,    1,    2,    2,    2,    1,    1, None, None],
+		[None, None, None,    1,    1,    2,    1,    1, None, None, None],
+		[None, None, None, None,    1,    1,    1, None, None, None, None],
 		[None, None, None, None, None,    1, None, None, None, None, None],
 	]
-	COLOR = (1, 121, 111)
+	COLOR = hex_to_rgb('001f08ff')
+
+
+class Bush(Tree):
+	MATRIX = [
+		[1.3, 1.2, 1.1, 1.9],
+		[  1, 1.5, 1.3,   1],
+		[1.4, 1.2,   2, 1.3],
+		[  1, 1.6,   1, 1.8],
+	]
+	COLOR = hex_to_rgb('00380eff')
+
+	def __init__(self, *args, **kwargs):
+		super(Bush, self).__init__(*args, **kwargs)
+		self.MATRIX = shuffle_matrix(self.MATRIX)
+
+
+class Stone(Thing):
+	pass
+
+
+class Rock(Stone):
+	MATRIX = [
+		[ 1.2, 1.3, 1.2, 2.7, 1.3, 1.9, 1.0, 1.8, 1.3],
+		[ 1.6, 1.1, 2.3, 2.2, 2.9, 1.2, 1.5, 1.4, 1.2],
+		[ 1.8, 2.9, 2.1, 3.7, 2.5, 2.9, 1.5, 1.3, 1.7],
+		[ 2.3, 2.4, 2.8, 2.7, 2.3, 2.2, 2.5, 1.1, 1.2],
+		[ 1.1, 2.2, 2.0, 3.9, 2.7, 2.9, 1.5, 1.7, 1.0],
+		[ 1.3, 1.6, 2.2, 2.3, 2.5, 1.8, 1.2, 1.3, 1.4],
+		[ 1.1, 1.8, 1.1, 2.9, 1.7, 1.8, 1.2, 1.1, 1.5],
+		[ 1.0, 1.9, 1.8, 1.7, 1.9, 1.4, 1.1, 1.3, 1.6],
+		[ 1.3, 1.1, 1.9, 1.6, 1.4, 1.5, 1.4, 1.9, 1.1],
+	]
+	COLOR = hex_to_rgb('2d3939ff')
+
+	def __init__(self, *args, **kwargs):
+		super(Rock, self).__init__(*args, **kwargs)
+		self.MATRIX = shuffle_matrix(self.MATRIX)
+
+
+class Gravel(Stone):
+	MATRIX = [[1]]
+	COLOR = hex_to_rgb('232424ff')
